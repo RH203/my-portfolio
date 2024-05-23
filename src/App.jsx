2e809531cb44765505766783248add34bcdc5976
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-23 23:01:54 +0700

    chore: Add new pages to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { FooTer, NavBar } from "./components";
+import { AboutMe, MyProject, PersonalAssistant } from "./pages";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gray-300 dark:bg-gray-900">
       <NavBar />
+      <AboutMe />
+      <MyProject />
+      <PersonalAssistant />
       <FooTer />
     </div>
   );
