c5d4bcc0bd284c7502eb0eeb7c78a6ccabd0d811
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-24 19:53:47 +0700

    chore: Update AboutMe page component layout and styling

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ import TypeIt from "typeit-react";
 
 export default function AboutMe() {
   return (
-    <Card className="w-10/12 mx-auto my-16 bg-white dark:bg-gray-300 shadow-xl text-gray-900 p-10 grid grid-rows-1">
+    <Card className="w-10/12 mx-auto my-14 bg-white dark:bg-gray-300 shadow-xl text-gray-900 p-10 grid grid-rows-1">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 ">
         <div className="">
           <img
@@ -13,10 +13,10 @@ export default function AboutMe() {
             alt="nature image"
           />
         </div>
-        <div className="flex lg:justify-start lg:items-start flex-col col-span-2 my-auto ml-5 gap-8 md:justify-center md:items-center">
+        <div className="flex lg:justify-start lg:items-start flex-col col-span-2 md:col-span-1 sm:col-span-1 my-auto ml-5 gap-8 md:justify-center md:items-center">
           <div>
             <header>
-              <h3 className="font-bold text-6xl font-poppins">Welcome to</h3>
+              <h3 className="font-bold text-5xl font-poppins">Welcome to</h3>
             </header>
 
             <div className="">
